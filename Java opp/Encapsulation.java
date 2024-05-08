84712c52af04a0655e72d1bceb055bf02f944fc6
Mostofa Kamal <mostofakamal3636@gmail.com>
2024-05-08 21:54:34 +0600

    JavaOOP

---
+++ Java opp/Encapsulation.java
@@ -0,0 +1,17 @@
+package oop;
+
+public class Encapsulation {
+    private int accnoNumber;
+    private String name;
+    private  int amount;
+
+    void setAccnt( int accnoNumber ){
+        this.accnoNumber = accnoNumber;
+    }
+    int getAccnt(){
+        return  accnoNumber;
+    }
+
+
+
+}
---
+++ Java opp/StudentsMain.java
@@ -0,0 +1,11 @@
+package oop;
+
+public class StudentsMain {
+    public static void main(String[] args) {
+        Students std1 = new Students();
+        std1.sid = 1;
+        std1.sname = "alamin";
+        std1.gen = 'M';
+        std1.display();
+    }
+}
