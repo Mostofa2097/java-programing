65e228b579a6da7ed393c34d62b0196159896be9
Mostofa Kamal <mostofakamal3636@gmail.com>
2024-05-21 19:07:09 +0600

    TodaysDinner

---
+++ src/main/java/assignment2/RelationalOperator.java
@@ -0,0 +1,18 @@
+package assignment2;
+
+public class RelationalOperator {
+    public static void main(String[] args) {
+        int a=6, b=5;
+        if(a<b){
+            System.out.println("b is big");
+        } else if (a>b) {
+            System.out.println("a is big");
+
+        }
+        else {
+            System.out.println("a == b");
+
+        }
+
+    }
+}
